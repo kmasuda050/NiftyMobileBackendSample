c33ce35a47c58248c38d4905289a581bd668864b
matsuko <matsuko@mononoco.com>
2014-01-22 18:52:04 +0900

    add iOS ApplePushNotificationService Sample

---
+++ iOS/ApplePushNotificationService/ApplePushNotificationService/AppDelegate.m
@@ -0,0 +1,65 @@
+//
+//  AppDelegate.m
+//  ApplePushNotificationService
+//
+//  Created by matsuko on 2014/01/22.
+//  Copyright (c) 2014年 mononoco. All rights reserved.
+//
+
+#import <NCMB/NCMB.h>
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    // Override point for customization after application launch.
+    [NCMB setApplicationKey:@"application_key" clientKey:@"client_key"];
+    
+    [[UIApplication sharedApplication]
+     registerForRemoteNotificationTypes:
+    (UIRemoteNotificationTypeBadge|
+     UIRemoteNotificationTypeSound|
+     UIRemoteNotificationTypeAlert)];
+
+    return YES;
+}
+
+// 配信端末情報を登録する。
+- (void)application:(UIApplication *)application
+didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    NCMBInstallation *currentInstallation = [NCMBInstallation currentInstallation];
+    [currentInstallation setDeviceTokenFromData:deviceToken];
+    [currentInstallation save];
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
---
+++ iOS/ApplePushNotificationService/ApplePushNotificationService/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ApplePushNotificationService
+//
+//  Created by matsuko on 2014/01/22.
+//  Copyright (c) 2014年 mononoco. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
